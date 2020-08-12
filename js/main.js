6f167667cda027c0eda9de7384ad4c7c551ffdbb
Koliaha <haquanghuy@mail.ru>
2020-08-12 21:54:54 +0300

    commit new

--- js/main.js
+++ js/main.js
@@ -1,42 +1,75 @@
-        var mySwiper = new Swiper('.swiper-container', {
-            loop: true,
-            pagination: {
-                el: '.swiper-pagination',
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-            scrollbar: {
-                el: '.swiper-scrollbar',
-            },
-        })
-        var prevScrollpos = window.pageYOffset;
-        window.onscroll = function () {
-            var currentScrollPos = window.pageYOffset;
-            if (prevScrollpos > currentScrollPos) {
-                document.getElementById("navbar").style.top = "113px";
-            } else {
-                document.getElementById("navbar").style.top = "-150px";
-            }
-            prevScrollpos = currentScrollPos;
+var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+})
+var prevScrollpos = window.pageYOffset;
+window.onscroll = () => {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "113px";
+    } else {
+        document.getElementById("navbar").style.top = "-150px";
+    }
+    prevScrollpos = currentScrollPos;
+    document.querySelector("#display-drop").style.display = "none"
+    document.querySelector(".copy").style.display = "none"
+    document.querySelector(".callback-modal").style.display = "none"
+}
+
+
+function display(x){
+    if (x.style.display == "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+document.querySelector('.category').addEventListener('click', (e) => {
+    display(document.querySelector("#display-drop"))
+})
+document.querySelector('.mail-copy').addEventListener('click', (e) => {
+    display(document.querySelector(".copy"))
+})
+
+document.querySelector('.call').addEventListener('mouseover', (e) => {
+    document.querySelector(".callback-modal").style.display = "block"
+})
+document.querySelector('.close').addEventListener('click', (e) => {
+    document.querySelector(".callback-modal").style.display = "none"
+})
+
+document.querySelector('.plus').addEventListener('click', (e) => {
+    let x = document.querySelectorAll('.complex-more')
+    x.forEach((el) => {
+        if (el.style.display == "block") {
+            el.style.display = "none";
+            e.target.innerHTML = '+'
+        } else {
+            el.style.display = "block";
+            e.target.innerHTML = 'X'
         }
+    })
 
-        document.querySelector('.category').addEventListener('click', (e) => {
-            var x = document.querySelector("#display-drop");
-            if (x.style.display == "block") {
-                x.style.display = "none";
-            } else {
-                x.style.display = "block";
-            }
-        })
+})
+document.querySelector('.copy').addEventListener('click', (e) => {
+    console.log('copy')
+     document.querySelector(".copymail-field").select();
+  document.execCommand("copy");
+})
 
-        document.querySelector('.mail-copy').addEventListener('click', (e) => {
-            var x = document.querySelector(".copy");
-            if (x.style.display == "block") {
-                x.style.display = "none";
-            } else {
-                x.style.display = "block";
-            }
-        })
-   
+document.querySelectorAll('#display-drop ul li a').forEach((el)=>{
+    el.addEventListener('click', (elem)=>{
+        elem.preventDefault();
+        document.querySelector('.category').innerHTML = elem.target.innerHTML
+        display(document.querySelector("#display-drop"))
+    })
+})
